25fa1564508acdde1c2a893308d21b48b79fbf91
Siddharth Panicker <siddharthpanicker18@gnu.ac.in>
2021-02-02 06:08:31 +0000

    version 3

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Welcome to DEMO 1 Project</p>
+        <p className="p-3 text-light h3 bg-primary">Welcome to Project done with the help of DevOps"</p>
     </div>
 }
   
